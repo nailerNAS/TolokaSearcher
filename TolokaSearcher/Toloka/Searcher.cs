69a6a4bf4dfb9589b0fabdcb27cdadffb993230e
nailer <nailer@pm.me>
2018-11-16 09:57:27 +0200

    add searcher

---
+++ TolokaSearcher/Toloka/Searcher.cs
@@ -0,0 +1,20 @@
+﻿using Flurl;
+using Flurl.Http;
+using System.Collections.Generic;
+
+namespace TolokaSearcher.Toloka
+{
+    public class Searcher
+    {
+        private static string apiUrl = "https://toloka.to/api.php";
+
+        public List<TolokaResult> search (string query)
+        {
+            return apiUrl
+                .SetQueryParam("search", query)
+                .GetAsync()
+                .ReceiveJson<List<TolokaResult>>()
+                .Result;
+        }
+    }
+}
